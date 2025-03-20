36ac98c1a42f3e117a5ccd1e3208c093d020ce20
ibrahim nada <ibrahimnada149@gmail.com>
2025-03-20 02:15:40 +0000

    feat(setupK3s): changing kubectl permissions, adding auto complete

--- p2/scripts/setupK3s.sh
+++ p2/scripts/setupK3s.sh
@@ -3,6 +3,17 @@
 # Install k3s
 curl -sfL https://get.k3s.io | sh -
 
+sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+
+alias k='kubectl'
+
+kubectl apply -f deployment.yaml
+
+# bash Auto completion for kubectl & vagrant
+sudo apt-get install -y bash-completion
+echo "source <(kubectl completion bash)" >> ~/.bashrc
+source ~/.bashrc
+
 # Getting Cluster Token
 # TOKEN=$(cat /var/lib/rancher/k3s/server/token)
 
