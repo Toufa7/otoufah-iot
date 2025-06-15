7e33711873b78bd0f9c8b95a9e0b66b410a2bbfe
Toufa7 <otoufah@student.1337.ma>
2025-06-15 19:39:44 +0100

    feat(argocd): cluster setup scritp docker, k3d, kubectl, argocd cli

--- p3/scripts/init.sh
+++ p3/scripts/init.sh
@@ -1,29 +1,60 @@
-# https://argo-cd.readthedocs.io/en/stable/getting_started/
-
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl port-forward svc/argocd-server -n argocd 8080:443
-
-# Login through the UI admin:[$k get secrets argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | based64 -d]
-
-k exec -n argocd -it argocd-server-99c485944-m48sz -- /bin/bash
-    $ argocd login localhost:443 --insecure
-    $ rgocd repo add git@gitlab.com:Toufa7/inception-of-things.git --ssh-private-key-path  id_rsa
+#!/bin/bash
 
 
-# Installing argocd on host
+## Init
 
-curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+# Install Docker
+echo -e "\033[34m$(date +%T) [INFO] Installing Docker\033[0m\033[0m"
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# Install K3D
+echo -e "\033[34m$(date +%T) [INFO] Installing k3d\033[0m"
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+# Creating cluster
+echo -e "\033[34m$(date +%T) [INFO] Creating k3d cluster\033[0m"
+k3d cluster create iot 
+
+# Install Kubectl
+echo -e "\033[34m$(date +%T) [INFO] Installing kubectl\033[0m"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl\033[0m"
+
+# Install Argo CD CLI
+echo -e "\033[34m$(date +%T) [INFO] Installing Argo CD\033[0m"
+# curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
-rm argocd-linux-amd64
+# rm argocd-linux-amd64
 
-# CLI It's not fully functionall
+# Install Argo CD setup
+echo -e "\033[34m$(date +%T) [INFO] Argo CD Setup\033[0m"
+kubectl create namespace argocd
+kubectl create namespace dev
 
-argocd app create argocd-iot \
-  --repo git@gitlab.com:Toufa7/inception-of-things.git\
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl wait deployment argocd-server -n argocd --for=condition=Available=True --timeout=30s
+nohup kubectl port-forward svc/argocd-server -n argocd 8080:443 > /dev/null 2>&1 &
+
+# Argo CD Credentials
+echo -e "\033[34m$(date +%T) [INFO] Argo CD Credentials\033[0m"
+kubectl get secrets argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d && echo
+
+# Login to Argo CD
+echo -e "\033[34m$(date +%T) [INFO] Logging in to Argo CD\033[0m"
+argocd login localhost:8080 --insecure
+argocd app create argocd-iota \
+  --repo https://gitlab.com/Toufa7/inception-of-things.git\
   --path p2/ \
   --dest-server https://kubernetes.default.svc \
   --dest-namespace default \
   --project default \
   --directory-recurse \
-  --sync-policy automated \
+  --sync-policy automated
+
+# Apply Kubernetes manifests
+echo -e "\033[34m$(date +%T) [INFO] Applying Kubernetes manifests\033[0m"
+kubectl apply -f ../confs/application.yaml
+
+echo -e "\033[34m$(date +%T) [INFO] Setup complete\033[0m"
+
+# k exec -n argocd -it argocd-server-99c485944-m48sz -- /bin/bash
+# $ argocd repo add https://gitlab.com/Toufa7/inception-of-things.git --ssh-private-key-path  id_rsa
