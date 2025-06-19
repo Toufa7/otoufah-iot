5677c952388bf3a633ddea3734b47bffa7d2d0fe
Toufa7 <otoufah@student.1337.ma>
2025-06-20 00:45:36 +0100

    fix(bugs): ingress routing + replicas + forwarding traffic from host

--- p2/scripts/setupK3s.sh
+++ p2/scripts/setupK3s.sh
@@ -6,7 +6,8 @@ sudo chmod 644 /etc/rancher/k3s/k3s.yaml
 kubectl apply -f /home/vagrant/confs
 
 # bash Auto completion for kubectl & vagrant
-sudo apt-get install -y bash-completion
+sudo apt install -y bash-completion
+sudo apt install net-tools
 echo "alias k='kubectl'" >> ~/.bashrc
 echo "source <(kubectl completion bash)" >> ~/.bashrc
 source ~/.bashrc
--- p3/scripts/init.sh
+++ p3/scripts/init.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-## Init
-
 # Install Docker
 echo -e "\033[34m$(date +%T) [INFO] Installing Docker\033[0m\033[0m"
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
@@ -20,9 +18,9 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 
 # Install Argo CD CLI
 echo -e "\033[34m$(date +%T) [INFO] Installing Argo CD\033[0m"
-# curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
-# rm argocd-linux-amd64
+rm argocd-linux-amd64
 
 # Install Argo CD setup
 echo -e "\033[34m$(date +%T) [INFO] Argo CD Setup\033[0m"
